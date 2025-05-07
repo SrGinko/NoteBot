38a14a76ab1af8e84a9395903efa4b1ee20a7e98
SrGinko <91520452+SrGinko@users.noreply.github.com>
2025-05-07 14:27:50 -0300

    adição de uma fução de bandeja do aplicativo

--- src/main.js
+++ src/main.js
@@ -1,18 +1,37 @@
-const { app , BrowserWindow, ipcMain, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, Tray, Menu, Notification } = require('electron');
+
 const path = require('path');
 
-const creatWindow = () => {
-    const win = new BrowserWindow({
-        minwidth: 800,
-        minheight: 600,
-        simpleFullscreen:true,
+let win
+
+function mostrarNotificacao(titulo, mensagem) {
+    const notificacao = new Notification({
+        title: titulo,
+        body: mensagem,
+        icon: path.join(__dirname, '../', 'Images', 'icos', 'app_icon.ico'),
+    })
+
+    return notificacao.show()
+}
+
+app.whenReady().then(() => {
+
+    win = new BrowserWindow({
+        minWidth: 1200,
+        minHeight: 800,
+        width: 1200,
+        height: 800,
+        simpleFullscreen: true,
         backgroundColor: '#1F1F1F',
         frame: false,
+        icon: path.join(__dirname, '../', 'Images', 'icos', 'app_icon.ico'),
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
+
+    const tray = new Tray(path.join(__dirname, '../', 'Images', 'icos', 'app_icon.ico'));
 
     win.loadFile('./src/index.html');
 
@@ -31,16 +50,38 @@ const creatWindow = () => {
                 break;
         }
     })
-}
 
-app.whenReady().then(() => {
-    creatWindow();
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Abrir', click: () => { win.show() } },
+        { label: 'Fechar', click: () => { win.close() } },
+        { label: 'Sair', click: () => {app.isQuitting = true, app.quit() } }
+    ])
+
+    tray.setToolTip('NotBot')
+    tray.setContextMenu(contextMenu)
+
+    tray.on('double-click', () => {
+        win.show()
+    })
+
+    win.on('close', (event) => {
+        if (!app.isQuitting) {
+            event.preventDefault()
+            mostrarNotificacao('NotBot', 'O NotBot está minimizado na bandeja do sistema. Clique duas vezes para abrir.')
+            win.hide()
+        }
+
+        return false
+    })
 })
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', (event) => {
+    event.preventDefault()
+    mostrarNotificacao('NotBot', 'O NotBot está minimizado na bandeja do sistema. Clique duas vezes para abrir.')
+})
+
+app.on('close', ()=>{
+    app.quit()
 })
 
 // Menu.setApplicationMenu(null)
